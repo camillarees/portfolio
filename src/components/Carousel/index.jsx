ff8e7c3d15846c39528d9ca59d6ac9cff17a0ed0
Camilla Rees <camilla.c.rees@gmail.com>
2023-02-23 14:31:02 -0800

    added plant space success images

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -17,58 +17,60 @@ const useStyles = createStyles((_theme, _params, getRef) => ({
 }));
 
 const PlantSpaceCarousel = () => {
-    const plantSpaceImages = [
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/plantSpace-UML.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/signup.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/signin.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/createPlant.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/updatePlant.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/getAllOrders.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/getAllPlants.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/addToCart.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/placeAnOrder.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/readOneOrder.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/updateOrder.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/chatServer2.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/serverTests.png',
-                'https://github.com/CHSMD/plant.space/raw/main/assets/img/authRouteTests.png',
-      
+    const images = [
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/plantSpace-UML.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/signup.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/signin.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/chatServer2.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/createPlant.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/updatePlant.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/getAllOrders.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/getAllPlants.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/addToCart.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/placeAnOrder.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/updateOrder.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/serverTests.png',
+        'https://github.com/CHSMD/plant.space/raw/main/assets/img/authRouteTests.png',
+
     ];
 
-    const { classes } = useStyles();
-    const slides = plantSpaceImages.map((item) => (
-        <Carousel.Slide key={item[0]}>
-            <Image src={item[0]} />
-        </Carousel.Slide>
-    ));
+const { classes } = useStyles();
+const slides = images.map((url) => (
+    <Carousel.Slide key={url}>
+        <Image
+            src={url} />
+    </Carousel.Slide>
 
-    return (
-        <Carousel
-            classNames={classes}
-            sx={{ maxWidth: 320 }}
-            mx="auto"
-            withIndicators
-            height={200}
-            styles={{
-                indicator: {
-                    width: 12,
-                    height: 4,
-                    transition: 'width 250ms ease',
-                    '&[data-active]': {
-                        width: 40,
-                        control: {
-                            '&[data-inactive]': {
-                                opacity: 0,
-                                cursor: 'default',
-                            },
+));
+
+return (
+    <Carousel
+        classNames={classes}
+        sx={{ maxWidth: '100%' }}
+        mx="auto"
+        withIndicators
+        height={400}
+        slideGap='md'
+        styles={{
+            indicator: {
+                width: 12,
+                height: 4,
+                transition: 'width 250ms ease',
+                '&[data-active]': {
+                    width: 40,
+                    control: {
+                        '&[data-inactive]': {
+                            opacity: 0,
+                            cursor: 'default',
                         },
                     },
                 },
-            }}
-        >
-            {slides}
-        </Carousel>
-    );
+            },
+        }}
+    >
+        {slides}
+    </Carousel>
+);
 }
 
 export default PlantSpaceCarousel;
