66e488057a14532d2f7d4c93808c30850025ae53
Camilla Rees <camilla.c.rees@gmail.com>
2023-03-22 19:08:32 -0700

    quick test

--- src/components/Video/CityVideo.jsx
+++ src/components/Video/CityVideo.jsx
@@ -11,9 +11,8 @@ const CityVideo = () => {
                     height="315"
                     src="https://www.youtube.com/embed/H8pqJxt56G4"
                     title="city explorer"
-                    frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;   picture-in-picture; web-share"
-                    allowfullscreen>
+                    allowFullScreen>
 
                 </iframe>
             </AspectRatio>
