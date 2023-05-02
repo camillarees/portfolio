30646aa66dc8cffb0f3583af80c1a857105b1856
Camilla Rees <camilla.c.rees@gmail.com>
2023-05-02 13:08:51 -0700

    updated timeline info

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -16,32 +16,32 @@ const MyStory = () => {
         whileInView={{ opacity: 1, y: 5 }}
         viewport={{ once: true }}
         >
-<Timeline color="indigo" active={5} bulletSize={24} lineWidth={2}>
-<Timeline.Item title="Coding Instructor">
-    <Text color="dimmed" size="sm">CodeFu</Text>
+<Timeline color="indigo" active={6} bulletSize={24} lineWidth={2}>
+<Timeline.Item title="Code Instructor">
+    <Text color="dimmed" size="sm">CodeFu | CodeX Academy</Text>
     <Text size="xs" mt={4}>March 2023</Text>
 </Timeline.Item>
 <Timeline.Item title="Full-stack JavaScript Development Certification">
     <Text color="dimmed" size="sm">Code Fellows</Text>
-    <Text size="xs" mt={4}>February 3rd, 2023</Text>
+    <Text size="xs" mt={4}>February 2023</Text>
 </Timeline.Item>
-<Timeline.Item title="Enrolled in Code Fellows bootcamp">
+<Timeline.Item title="Enrolled at Code Fellows">
     <Text color="dimmed" size="sm">Code Fellows</Text>
-    <Text size="xs" mt={4}>June 1, 2022</Text>
+    <Text size="xs" mt={4}>May 2022</Text>
 </Timeline.Item>
 
-<Timeline.Item title="Nonprofit Program Manager">
+<Timeline.Item title="Program and Marketing Manager">
     <Text color="dimmed" size="sm">Utah State University </Text>
-    <Text size="xs" mt={4}>October 2019 - November 2022</Text>
+    <Text size="xs" mt={4}>October 2019</Text>
 </Timeline.Item>
 
-<Timeline.Item title="BS in Family Studies from Brigham Young University">
+<Timeline.Item title="B.S in Family Studies and Human Development, Dean's List">
     <Text color="dimmed" size="sm">Brigham Young University</Text>
     <Text size="xs" mt={4}>December 2019</Text>
 </Timeline.Item>
 
-<Timeline.Item title="Photographer and Relationship Blogger">
-    <Text color="dimmed" size="sm">Founder</Text>
+<Timeline.Item title="Photographer, Copywriter, and Content Creator">
+    <Text color="dimmed" size="sm">Self-Employed</Text>
     <Text size="xs" mt={4}>2015-2021</Text>
 </Timeline.Item>
 </Timeline>
